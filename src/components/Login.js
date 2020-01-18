4d8b21fc335c3b0d809e377622fd19cc42990917
Nishit Jain <nishitj077@gmail.com>
2020-01-18 21:26:03 +0530

    add login

---
+++ src/components/Login.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./Login.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGraduationCap,
+  faLock,
+  faEnvelopeOpen
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+class Login extends Component {
+  render() {
+    return (
+      <main className="main">
+        <section>
+          <div>
+            <p>
+              Login to <span className="coloured">Maillux</span>
+            </p>
+            <div>
+              <input type="email" placeholder="email" required></input>
+              <input type="password" placeholder="password" required></input>
+            </div>
+            <button>Submit</button>
+            <Link to="/forgot">Forgot Password?</Link>
+            <p>Not Registered Yet ?</p>
+            <Link to="/register">Create New Account</Link>
+          </div>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default Login;
