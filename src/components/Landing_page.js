5dd096277b045297887b2c9865ba4b98e0ba904a
Nishit Jain <nishitj077@gmail.com>
2020-01-19 18:50:21 +0530

    update

--- src/components/Landing_page.js
+++ src/components/Landing_page.js
@@ -1,95 +1,108 @@
-import React, { Component } from "react";
-import "./Landing_page.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Component } from 'react';
+import './Landing_page.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faGraduationCap,
-  faLock,
-  faEnvelopeOpen
-} from "@fortawesome/free-solid-svg-icons";
+    faGraduationCap,
+    faLock,
+    faEnvelopeOpen
+} from '@fortawesome/free-solid-svg-icons';
 
-import { Route, Link } from "react-router-dom";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { Link } from 'react-router-dom';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 class Landing_Page extends Component {
-  render() {
-    return (
-      <main>
-        {" "}
-        <header>
-          <nav>
-            <div className="Left">
-              <div className="logo-container">
-                <FontAwesomeIcon
-                  icon={faGraduationCap}
-                  flip="horizontal"
-                  className="logo"
-                />
-                {""}
-                <div>Maillux</div>
-              </div>
-              <div className="nav-links">
-                <a href="/google">Learn</a>
-                <a href="/google">Create</a>
-              </div>
-            </div>
-            <div className="Right">
-              <div>
-                <FontAwesomeIcon icon={faLock} className="lock" />
-                <Link to="/login">Login</Link>
-              </div>
-              <div className="signup-button">
-                <div>Sign Up</div>
-              </div>
-            </div>
-          </nav>
-        </header>
-        <section>
-          <article>
-            <div>
-              <span className="light">Learn</span>{" "}
-              <span className="mild">
-                something <span className="light">new </span>everyday,
-                <br />
-                from
-              </span>{" "}
-              <span className="light"> e-mail </span>{" "}
-              <span className="mild"> courses </span>{" "}
-            </div>
-            <div>
-              Subscribe to email courses <span className="light">or</span>{" "}
-              Create your own email course, fill the content, publish it to
-              share your knowledge with the world.
-            </div>
-            <div>
-              <a href="/courses">Start Learning</a>
+    render() {
+        return (
+            <main>
+                {' '}
+                <header>
+                    <nav>
+                        <div className='Left'>
+                            <div className='logo-container'>
+                                <FontAwesomeIcon
+                                    icon={faGraduationCap}
+                                    flip='horizontal'
+                                    className='logo'
+                                />
+                                {''}
+                                <div>Maillux</div>
+                            </div>
+                            <div className='nav-links'>
+                                <Link to='/courses'>Learn</Link>
+                                <Link to='/register'>Create</Link>
+                            </div>
+                        </div>
+                        <div className='Right'>
+                            <Link to='/login'>
+                                {' '}
+                                <div>
+                                    <FontAwesomeIcon
+                                        icon={faLock}
+                                        className='lock'
+                                    />
+                                    {'Login'}
+                                </div>
+                            </Link>
 
-              <a href="/courses">Sign Up to create a course</a>
-            </div>
-          </article>
-          <aside>
-            <div>
-              <img src="teach.jpeg" />
-            </div>
-          </aside>
-        </section>
-        <div className="connect">
-          <div>Connect with us</div>
+                            <Link to='/register'>
+                                {' '}
+                                <div className='signup-button'>
+                                    <div>Sign Up</div>
+                                </div>
+                            </Link>
+                        </div>
+                    </nav>
+                </header>
+                <section className='main-section'>
+                    <article>
+                        <div>
+                            <span className='light'>Learn</span>{' '}
+                            <span className='mild'>
+                                something <span className='light'>new </span>
+                                everyday,
+                                <br />
+                                from
+                            </span>{' '}
+                            <span className='light'> e-mail </span>{' '}
+                            <span className='mild'> courses </span>{' '}
+                        </div>
+                        <div>
+                            Subscribe to email courses{' '}
+                            <span className='light'>or</span> Create your own
+                            email course, fill the content, publish it to share
+                            your knowledge with the world.
+                        </div>
+                        <div>
+                            <Link to='/courses'>Start Learning</Link>
+                            <Link to='/register'>
+                                Sign Up to create a course
+                            </Link>
+                        </div>
+                    </article>
+                    <aside>
+                        <div>
+                            <img src='teach.jpeg' />
+                        </div>
+                    </aside>
+                </section>
+                <div className='connect'>
+                    <div>Connect with us</div>
 
-          <div>
-            <div>
-              <FontAwesomeIcon icon={faEnvelopeOpen} />
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faGithub} />
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faLinkedin} />
-            </div>
-          </div>
-        </div>
-        <footer> © Maillux 2020</footer>
-      </main>
-    );
-  }
+                    <div>
+                        <div>
+                            <FontAwesomeIcon icon={faEnvelopeOpen} />
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faGithub} />
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </div>
+                    </div>
+                </div>
+                <footer> © Maillux 2020</footer>
+            </main>
+        );
+    }
 }
 
 export default Landing_Page;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,43 @@
-import React, { Component } from "react";
-import "./Login.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Component } from 'react';
+import './Login.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faGraduationCap,
-  faLock,
-  faEnvelopeOpen
-} from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+    faGraduationCap,
+    faLock,
+    faEnvelopeOpen
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 class Login extends Component {
-  render() {
-    return (
-      <main className="main">
-        <section>
-          <div>
-            <p>
-              Login to <span className="coloured">Maillux</span>
-            </p>
-            <div>
-              <input type="email" placeholder="email" required></input>
-              <input type="password" placeholder="password" required></input>
-            </div>
-            <button>Submit</button>
-            <Link to="/forgot">Forgot Password?</Link>
-            <p>Not Registered Yet ?</p>
-            <Link to="/register">Create New Account</Link>
-          </div>
-        </section>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main className='main'>
+                <section className='login-section'>
+                    <div>
+                        <p>
+                            Login to <span className='coloured'>Maillux</span>
+                        </p>
+                        <div>
+                            <input
+                                type='email'
+                                placeholder='email'
+                                required
+                            ></input>
+                            <input
+                                type='password'
+                                placeholder='password'
+                                required
+                            ></input>
+                        </div>
+                        <button>Submit</button>
+                        <Link to='/forgot'>Forgot Password?</Link>
+                        <p>Not Registered Yet ?</p>
+                        <Link to='/register'>Create New Account</Link>
+                    </div>
+                </section>
+            </main>
+        );
+    }
 }
 
 export default Login;
