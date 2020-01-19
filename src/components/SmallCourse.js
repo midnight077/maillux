9f023198673421aaf69b38177c7e06438b687ed5
Nishit Jain <nishitj077@gmail.com>
2020-01-19 18:51:00 +0530

    add smallCourse Component

---
+++ src/components/SmallCourse.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+class SmallCourse extends Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+        this.colors = [
+            ['#01579b', '#03a9f4'],
+            ['#880e4f', '#e91e63'],
+            ['#4a148c', '#673ab7'],
+            ['#b71c1c', '#f44336'],
+            ['#006064', '#00bcd4'],
+            ['#e65100', '#ff9800'],
+            ['#004d40', '#009688'],
+            ['#bf360c', '#ff5722'],
+            ['#1a237e', '#3f51b5'],
+            ['#1b5e20', '#4caf50']
+        ];
+    }
+    render() {
+        return (
+            <div
+                className='course'
+                style={{
+                    background: this.colors[this.props.col][0]
+                }}
+            >
+                <div style={{ fontSize: `${22}px`, color: 'white' }}>
+                    {this.props.title}
+                </div>
+                <div
+                    className='info'
+                    style={{
+                        background: this.colors[this.props.col][1]
+                    }}
+                >
+                    <div>By {this.props.author}</div>
+                    <div>
+                        Duration : <strong>{this.props.duration}</strong> Days
+                    </div>
+                    <div>
+                        Subscribers : <strong>{this.props.subs}</strong>{' '}
+                    </div>
+                    <div>
+                        Published on : <strong>{this.props.published}</strong>
+                    </div>
+                    <div>{this.props.description.slice(0, 131) + ' ...'}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SmallCourse;
