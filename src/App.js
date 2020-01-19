bc8c2afbdfd88576d386b91ddcf6055cb520cbc4
Nishit Jain <nishitj077@gmail.com>
2020-01-19 18:49:50 +0530

    add courses

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Landing_Page from './components/Landing_page';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Courses from './components/Courses';
 import { Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Route exact path='/' component={Landing_Page} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/register' component={Signup} />
+            <Route exact path='/courses' component={Courses} />
         </div>
     );
 }
