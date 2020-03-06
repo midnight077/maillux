ae63acc65df6c5a76d265b915c55ac3626ca7bff
Nishit Jain <nishitj077@gmail.com>
2020-03-07 00:15:29 +0530

    add auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,82 @@
-import React, { Component } from 'react';
-import './Login.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from "react";
+import "./Login.css";
+import axios from "../axios";
+import { Link, Redirect } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faGraduationCap,
-    faLock,
-    faEnvelopeOpen
-} from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-class Login extends Component {
-    render() {
-        return (
-            <main className='main'>
-                <section className='login-section'>
-                    <div>
-                        <p>
-                            Login to <span className='coloured'>Maillux</span>
-                        </p>
-                        <div>
-                            <input
-                                type='email'
-                                placeholder='email'
-                                required
-                            ></input>
-                            <input
-                                type='password'
-                                placeholder='password'
-                                required
-                            ></input>
-                        </div>
-                        <button>Submit</button>
-                        <Link to='/forgot'>Forgot Password?</Link>
-                        <p>Not Registered Yet ?</p>
-                        <Link to='/register'>Create New Account</Link>
-                    </div>
-                </section>
-            </main>
-        );
+  faGraduationCap,
+  faLock,
+  faEnvelopeOpen
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+const Login = () => {
+  // state for email password login status error and form state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedin, setloggedin] = useState(false);
+  const [error, setError] = useState({ isError: false, reason: "" });
+  const [loading, setLoading] = useState(false);
+  const [invalidForm, setFormInvalidity] = useState(true);
+  // submit handler
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    const res = await axios.post("api/user/login", {
+      email: email,
+      password: password
+    });
+    // error handler
+    if (!res.data.error) {
+      localStorage.setItem("auth-token", res.data.token);
+      setloggedin(true);
+    } else {
+      setLoading(false);
+      setError({ isError: res.data.error, reason: res.data.reason });
     }
-}
+  };
+  // use effect hook
+  useEffect(() => {
+    const token = localStorage.getItem("auth-token");
+    if (token) {
+      setloggedin(true);
+    }
+    if (email.trim(" ").length > 0 && password.trim(" ").length > 0) {
+      setFormInvalidity(false);
+    } else {
+      setFormInvalidity(true);
+    }
+  }, [email, password]);
+  return (
+    <main className="main">
+      <section className="login-section">
+        <div>
+          <p>
+            Login to <span className="coloured">Maillux</span>
+          </p>
+          <div>
+            <input
+              type="email"
+              placeholder="email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <input
+              type="password"
+              placeholder="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </div>
+          <button onClick={handleSubmit}>Submit</button>
+          <Link to="/forgot">Forgot Password?</Link>
+          <p>Not Registered Yet ?</p>
+          <Link to="/register">Create New Account</Link>
+          {loggedin && <Redirect to="/courses" />}
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,47 +1,86 @@
-import React, { Component } from 'react';
-import './Signup.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import "./Signup.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faGraduationCap,
-    faLock,
-    faEnvelopeOpen
-} from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-class Login extends Component {
-    render() {
-        return (
-            <main className='main'>
-                <section className='signup-section'>
-                    <div>
-                        <p>
-                            Sign Up to <span className='coloured'>Maillux</span>
-                        </p>
-                        <div>
-                            <input
-                                type='text'
-                                placeholder='Username'
-                                required
-                            ></input>
-                            <input
-                                type='email'
-                                placeholder='email'
-                                required
-                            ></input>
-                            <input
-                                type='password'
-                                placeholder='password'
-                                required
-                            ></input>
-                        </div>
-                        <button>Submit</button>
-                        <p>Already Registerd?</p>
-                        <Link to='/login'>Login</Link>
-                    </div>
-                </section>
-            </main>
-        );
+  faGraduationCap,
+  faLock,
+  faEnvelopeOpen
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { Link, Redirect } from "react-router-dom";
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedin, setloggedin] = useState(false);
+  const [error, setError] = useState({ isError: false, reason: "" });
+  const [invalidForm, setFormInvalidity] = useState(true);
+  // submit handler
+  const handleSubmit = async (e) => {
+    const res = await axios.post("/api/user/new", {
+      email,
+      password,
+      username
+    });
+    // error handler
+    if (!res.data.error) {
+      localStorage.setItem("auth-token", res.data.token);
+      setloggedin(true);
+    } else {
+      setError({ isError: res.data.error, reason: res.data.reason });
     }
-}
-
-export default Login;
+  };
+  // use effect hook
+  useEffect(() => {
+    const token = localStorage.getItem("auth-token");
+    if (token) {
+      setloggedin(true);
+    }
+    if (
+      email.trim(" ").length > 0 &&
+      password.trim(" ").length > 0 &&
+      username.trim(" ").length > 0
+    ) {
+      setFormInvalidity(false);
+    } else {
+      setFormInvalidity(true);
+    }
+  }, [email, password, username]);
+  return (
+    <main className="main">
+      <section className="signup-section">
+        <div>
+          <p>
+            Sign Up to <span className="coloured">Maillux</span>
+          </p>
+          <div>
+            <input
+              type="text"
+              placeholder="Username"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
+            <input
+              type="email"
+              placeholder="email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <input
+              type="password"
+              placeholder="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </div>
+          <button onClick={handleSubmit}>Submit</button>
+          <p>Already Registerd?</p>
+          <Link to="/login">Login</Link>
+          {loggedin && <Redirect to="/courses" />}
+        </div>
+      </section>
+    </main>
+  );
+};
+export default Signup;
