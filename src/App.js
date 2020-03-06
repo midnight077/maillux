98afbf2c00c2509d3770dc49b53dcd852365c215
Nishit Jain <nishitj077@gmail.com>
2020-03-07 00:15:23 +0530

    add auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import './App.css';
-import Landing_Page from './components/Landing_page';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Courses from './components/Courses';
-import Course from './components/Course';
-import { Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Landing_Page from "./components/Landing_page";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Courses from "./components/Courses";
+import Course from "./components/Course";
+import User from "./components/User";
+import { Route } from "react-router-dom";
 
 function App() {
-    return (
-        <div className='App'>
-            <Route exact path='/' component={Landing_Page} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Signup} />
-            <Route exact path='/courses' component={Courses} />
-            <Route exact path='/course/:id' component={Course} />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Route exact path="/" component={Landing_Page} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/user" component={User} />
+      <Route exact path="/register" component={Signup} />
+      <Route exact path="/courses" component={Courses} />
+      <Route exact path="/course/:id" component={Course} />
+    </div>
+  );
 }
 
 export default App;
