5f77cbcdd4590613e96cbd307b4fb5ef1fc86c1f
Nishit Jain <nishitj077@gmail.com>
2020-03-07 19:23:28 +0530

    add auth

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,101 +1,101 @@
-import React, { useState, useEffect } from 'react';
-import './Course.css';
-import { getCourseById } from '../utils/api';
+import React, { useState, useEffect } from "react";
+import "./Course.css";
+import { getCourseById } from "../utils/api";
 const Course = ({ match }) => {
-    const [data, setData] = useState({});
-    useEffect(() => {
-        (async () => {
-            const { data } = await getCourseById(match.params.id);
-            if (data) {
-                setData(data.data);
-            }
-        })();
-    }, [match.params.id]);
-    const hashCode = (s = '') =>
-        s.split('').reduce((a, b) => {
-            a = (a << 5) - a + b.charCodeAt(0);
-            return a & a;
-        }, 0);
-    const colors = [
-        ['#01579b', '#03a9f4'],
-        ['#880e4f', '#e91e63'],
-        ['#4a148c', '#673ab7'],
-        ['#b71c1c', '#f44336'],
-        ['#006064', '#00bcd4'],
-        ['#e65100', '#ff9800'],
-        ['#004d40', '#009688'],
-        ['#bf360c', '#ff5722'],
-        ['#1a237e', '#3f51b5'],
-        ['#1b5e20', '#4caf50']
-    ];
-    return (
-        <div>
-            {console.log(data)}
-            <nav
-                style={{
-                    background: colors[hashCode(data.author) % 10][1]
-                }}
-            >
-                <div
-                    style={{
-                        fontSize: '30px',
-                        color: 'black'
-                    }}
-                >
-                    {data.title}
-                </div>
-            </nav>
-            <div className='course_info'>
-                <p>
-                    Course Author : <strong>{data.author}</strong>
-                </p>
-                <p>
-                    Duration : <strong>{data.duration}</strong> Days
-                </p>
-                <p>
-                    Subscribers: <strong>{data.subscribers}</strong>
-                </p>
-                <p>
-                    Published On:{' '}
-                    <strong>
-                        {data.createdAt
-                            ? new Date(
-                                  +data.createdAt.slice(0, 4),
-                                  +data.createdAt.slice(5, 7) - 1,
-                                  +data.createdAt.slice(8, 10)
-                              )
-                                  .toDateString()
-                                  .slice(4)
-                            : ''}
-                    </strong>
-                </p>
-                <p>{data.description || 'No Description Available!'}</p>
-            </div>
-            <div>
-                <form className='subs_form'>
-                    <input
-                        type='text'
-                        placeholder='Enter your Name'
-                        style={{
-                            marginBottom: '2px'
-                        }}
-                    ></input>
-                    <input type='email' placeholder='Enter your email'></input>
-                    <submit
-                        style={{
-                            textAlign: 'center',
-                            cursor: 'pointer',
-                            padding: '20px',
-                            background: colors[hashCode(data.author) % 10][1],
-                            color: 'black',
-                            fontSize: '25px'
-                        }}
-                    >
-                        Subscribe
-                    </submit>
-                </form>
-            </div>
+  const [data, setData] = useState({});
+  useEffect(() => {
+    (async () => {
+      const { data } = await getCourseById(match.params.id);
+      if (data) {
+        setData(data.data);
+      }
+    })();
+  }, [match.params.id]);
+  const hashCode = (s = "") =>
+    s.split("").reduce((a, b) => {
+      a = (a << 5) - a + b.charCodeAt(0);
+      return a & a;
+    }, 0);
+  const colors = [
+    ["#01579b", "#03a9f4"],
+    ["#880e4f", "#e91e63"],
+    ["#4a148c", "#673ab7"],
+    ["#b71c1c", "#f44336"],
+    ["#006064", "#00bcd4"],
+    ["#e65100", "#ff9800"],
+    ["#004d40", "#009688"],
+    ["#bf360c", "#ff5722"],
+    ["#1a237e", "#3f51b5"],
+    ["#1b5e20", "#4caf50"]
+  ];
+  return (
+    <div>
+      {console.log(data)}
+      <nav
+        style={{
+          background: colors[hashCode(data.author) % 10][1]
+        }}
+      >
+        <div
+          style={{
+            fontSize: "30px",
+            color: "black"
+          }}
+        >
+          {data.title}
         </div>
-    );
+      </nav>
+      <div className="course_info">
+        <p>
+          Course Author : <strong>{data.author}</strong>
+        </p>
+        <p>
+          Duration : <strong>{data.duration}</strong> Days
+        </p>
+        <p>
+          Subscribers: <strong>{data.subscribers}</strong>
+        </p>
+        <p>
+          Published On:{" "}
+          <strong>
+            {data.createdAt
+              ? new Date(
+                  +data.createdAt.slice(0, 4),
+                  +data.createdAt.slice(5, 7) - 1,
+                  +data.createdAt.slice(8, 10)
+                )
+                  .toDateString()
+                  .slice(4)
+              : ""}
+          </strong>
+        </p>
+        <p>{data.description || "No Description Available!"}</p>
+      </div>
+      <div>
+        <form className="subs_form">
+          <input
+            type="text"
+            placeholder="Enter your Name"
+            style={{
+              marginBottom: "2px"
+            }}
+          ></input>
+          <input type="email" placeholder="Enter your email"></input>
+          <submit
+            style={{
+              textAlign: "center",
+              cursor: "pointer",
+              padding: "20px",
+              background: colors[hashCode(data.author) % 10][1],
+              color: "black",
+              fontSize: "25px"
+            }}
+          >
+            Subscribe
+          </submit>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default Course;
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,63 +1,82 @@
-import React, { useEffect, useState } from 'react';
-import './Courses.css';
-import { getCourses } from '../utils/api';
-import SmallCourse from './SmallCourse';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "./Courses.css";
+
+import axios from "../axios";
+import { getCourses } from "../utils/api";
+import SmallCourse from "./SmallCourse";
+import { Link } from "react-router-dom";
 const Courses = () => {
-    const hashCode = (s) =>
-        s.split('').reduce((a, b) => {
-            a = (a << 5) - a + b.charCodeAt(0);
-            return a & a;
-        }, 0);
-    const [courses, setCourses] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const courses = await getCourses();
-            setCourses(courses.data.data);
-        })();
-    }, []);
-    return (
-        <main>
-            <header>
-                <nav>
-                    <div style={{ fontSize: 25 + 'px', color: '#009c8e' }}>
-                        <Link style={{ color: '#009c8e' }} to='/'>
-                            Maillux
-                        </Link>
-                        <div>Courses</div>
-                    </div>
-                </nav>
-            </header>
-            <section className='courses-section'>
-                <div>
-                    {courses.map((item) => {
-                        return (
-                            <Link to={'/course/' + item._id} title={item.title}>
-                                <SmallCourse
-                                    title={item.title}
-                                    col={hashCode(item.author) % 10}
-                                    author={item.author}
-                                    duration={item.duration}
-                                    subs={item.subscribers.length}
-                                    published={new Date(
-                                        +item.createdAt.slice(0, 4),
-                                        +item.createdAt.slice(5, 7) - 1,
-                                        +item.createdAt.slice(8, 10)
-                                    )
-                                        .toDateString()
-                                        .slice(4)}
-                                    description={
-                                        item.description ||
-                                        'No Description Available'
-                                    }
-                                />
-                            </Link>
-                        );
-                    })}
-                </div>
-            </section>
-        </main>
-    );
+  const [loggedin, setLoggedin] = useState(true);
+  const fetchData = async () => {
+    const token = await localStorage.getItem("auth-token");
+    if (!token) {
+      setLoggedin(false);
+      return;
+    }
+    const response = await axios.get("/api/user", {
+      headers: { "x-access-token": token }
+    });
+    setUser(response.data);
+  };
+  const [user, setUser] = useState({});
+  console.log(user);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const hashCode = (s) =>
+    s.split("").reduce((a, b) => {
+      a = (a << 5) - a + b.charCodeAt(0);
+      return a & a;
+    }, 0);
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const courses = await getCourses();
+      setCourses(courses.data.data);
+    })();
+  }, []);
+  return (
+    <main>
+      <header>
+        <nav>
+          <div style={{ fontSize: 25 + "px", color: "#009c8e" }}>
+            <Link style={{ color: "#009c8e" }} to="/">
+              Maillux
+            </Link>
+            <div>Courses</div>
+            <Link style={{ color: "#009c8e" }} to="/create">
+              Create
+            </Link>
+          </div>
+        </nav>
+      </header>
+      <section className="courses-section">
+        <div>
+          {courses.map((item) => {
+            return (
+              <Link to={"/course/" + item._id} title={item.title}>
+                <SmallCourse
+                  title={item.title}
+                  col={hashCode(item.author) % 10}
+                  author={item.author == user.username ? "you" : item.author}
+                  duration={item.duration}
+                  subscribers={item.subscribers}
+                  published={new Date(
+                    +item.createdAt.slice(0, 4),
+                    +item.createdAt.slice(5, 7) - 1,
+                    +item.createdAt.slice(8, 10)
+                  )
+                    .toDateString()
+                    .slice(4)}
+                  description={item.description || "No Description Available"}
+                />
+              </Link>
+            );
+          })}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Courses;
