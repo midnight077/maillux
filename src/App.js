0cb667bc58fb20ea16006f401c1728a1d3890071
Nishit Jain <nishitj077@gmail.com>
2020-03-08 16:05:08 +0530

    add Switch

--- src/App.js
+++ src/App.js
@@ -7,18 +7,20 @@ import Courses from "./components/Courses";
 import Course from "./components/Course";
 import User from "./components/User";
 import Create from "./components/Create";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Landing_Page} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/user" component={User} />
-      <Route exact path="/register" component={Signup} />
-      <Route exact path="/courses" component={Courses} />
-      <Route exact path="/create" component={Create} />
-      <Route exact path="/course/:id" component={Course} />
+      <Switch>
+        <Route exact path="/" component={Landing_Page} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/user" component={User} />
+        <Route exact path="/register" component={Signup} />
+        <Route exact path="/courses" component={Courses} />
+        <Route exact path="/create" component={Create} />
+        <Route exact path="/course/:id" component={Course} />
+      </Switch>
     </div>
   );
 }
