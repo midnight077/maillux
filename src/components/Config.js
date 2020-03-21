65f8f451a0046eb343a163428e82778f0f3a8e2c
Nishit Jain <nishitj077@gmail.com>
2020-03-21 22:32:03 +0530

    add config page

--- src/components/Config.js
+++ src/components/Config.js
@@ -3,8 +3,19 @@ import axios from "../axios";
 import "./Config.css";
 import { Redirect, Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencilAlt, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 const Config = ({ match }) => {
   const [loggedin, setLoggedin] = useState(true);
+  const fetchCourse = () => {
+    user &&
+      user.courses &&
+      user.courses.forEach((item) => {
+        if (item._id == match.params.id) {
+          setCourse(item);
+        }
+      });
+  };
   const fetchData = async () => {
     const token = localStorage.getItem("auth-token");
     if (!token) {
@@ -13,29 +24,70 @@ const Config = ({ match }) => {
     const response = await axios.get("/api/user", {
       headers: { "x-access-token": token }
     });
+    response.data.courses &&
+      response.data.courses.forEach((item) => {
+        if (item._id == match.params.id) {
+          setCourse(item);
+        }
+      });
     setUser(response.data);
-    const crs = await axios.get("/api/course", {
-      headers: { "x-access-token": token }
-    });
-    setUser(response.data);
-    // user.courses.forEach((item) => {
-    //   console.log(1);
-    //   if (item._id == match.params.id) {
-    //     console.log(item._id);
-    //     setCourse(item);
-    //   }
-    // });
   };
 
   const [course, setCourse] = useState({});
   const [user, setUser] = useState({});
-  console.log(user);
-  console.log(course);
+  const [titleOpen, setTitleOpen] = useState(false);
   useEffect(() => {
+    console.log(1);
     fetchData();
   }, []);
 
-  return <>{!loggedin ? <Redirect to="/" /> : null}</>;
+  return (
+    <>
+      {!loggedin ? <Redirect to="/" /> : null}
+      <div className="config-title">
+        {!titleOpen ? (
+          <div>{course.title}</div>
+        ) : (
+          <div>
+            <input value={course.title} />
+          </div>
+        )}
+        <div onClick={(e) => setTitleOpen(!titleOpen)}>
+          {titleOpen ? (
+            <FontAwesomeIcon
+              icon={faCheckCircle}
+              className="config-title-icon"
+              style={{ color: "#375216" }}
+              title="save"
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faPencilAlt}
+              className="config-title-icon"
+              style={{ color: "#b12a00" }}
+              title="edit "
+            />
+          )}
+        </div>
+      </div>
+      <div className="config-material-holder">
+        {course &&
+          course.content &&
+          course.content.map((item, i) => (
+            <div className="config-material-day">
+              Day {i + 1} <br></br>
+              <div className="config-material-title">
+                Title <input placeholder="enter the Title" />{" "}
+              </div>
+              <div className="config-material-content">
+                Material <textarea placeholder="enter the day material" />{" "}
+              </div>
+              <button>save changes</button>
+            </div>
+          ))}
+      </div>
+    </>
+  );
 };
 
 export default Config;
