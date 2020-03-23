ba92857b00cafaf00f290e4b31364d51c7d1ee7b
Nishit Jain <nishitj077@gmail.com>
2020-03-23 17:38:34 +0530

    set config messages

--- src/components/Config.js
+++ src/components/Config.js
@@ -7,15 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencilAlt, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 const Config = ({ match }) => {
   const [loggedin, setLoggedin] = useState(true);
-  const fetchCourse = () => {
-    user &&
-      user.courses &&
-      user.courses.forEach((item) => {
-        if (item._id == match.params.id) {
-          setCourse(item);
-        }
-      });
-  };
+
   const fetchData = async () => {
     const token = localStorage.getItem("auth-token");
     if (!token) {
@@ -35,9 +27,9 @@ const Config = ({ match }) => {
 
   const [course, setCourse] = useState({});
   const [user, setUser] = useState({});
+  const [message, setMessage] = useState("");
   const [titleOpen, setTitleOpen] = useState(false);
   useEffect(() => {
-    console.log(1);
     fetchData();
   }, []);
 
@@ -49,10 +41,27 @@ const Config = ({ match }) => {
           <div>{course.title}</div>
         ) : (
           <div>
-            <input value={course.title} />
+            <input
+              value={course.title}
+              onChange={(e) => {
+                setCourse({ ...course, title: e.target.value });
+              }}
+            />
           </div>
         )}
-        <div onClick={(e) => setTitleOpen(!titleOpen)}>
+        <div
+          onClick={async (e) => {
+            if (titleOpen) {
+              const token = localStorage.getItem("auth-token");
+              const response = await axios.put(
+                `/api/courses/${match.params.id}/title`,
+                { title: course.title },
+                { headers: { "x-access-token": token } }
+              );
+            }
+            setTitleOpen(!titleOpen);
+          }}
+        >
           {titleOpen ? (
             <FontAwesomeIcon
               icon={faCheckCircle}
@@ -70,19 +79,127 @@ const Config = ({ match }) => {
           )}
         </div>
       </div>
+      {message && <div className="config-message">{message}</div>}
+      <div className="config-day-adder">
+        <p>Total Days : {course.content && course.content.length}</p>
+        <button
+          onClick={async (e) => {
+            const token = localStorage.getItem("auth-token");
+            const response = await axios.post(
+              `/api/courses/${match.params.id}/day`,
+              {},
+              { headers: { "x-access-token": token } }
+            );
+            setCourse(response.data.data);
+          }}
+        >
+          extend +{" "}
+        </button>
+      </div>
+      <div className="config-options">
+        <div className="config-publish">
+          <p>Publish</p>
+          <div
+            onClick={async (e) => {
+              const token = localStorage.getItem("auth-token");
+              const response = await axios.post(
+                `/api/courses/${match.params.id}`,
+                {},
+                { headers: { "x-access-token": token } }
+              );
+              if (response.data.data) {
+                setCourse(response.data.data);
+              }
+              if (response.data.err) {
+                setMessage(response.data.err);
+                setTimeout(() => {
+                  setMessage("");
+                }, 3000);
+              }
+            }}
+          >
+            {course && course.isPublished ? (
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className="config-title-icon"
+                style={{ color: "#375216" }}
+                title="save"
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faPencilAlt}
+                className="config-title-icon"
+                style={{ color: "#b12a00" }}
+                title="edit "
+              />
+            )}
+          </div>
+        </div>
+        <button className="config-delete-course">Delete this Course !</button>
+      </div>
+
       <div className="config-material-holder">
         {course &&
           course.content &&
           course.content.map((item, i) => (
             <div className="config-material-day">
-              Day {i + 1} <br></br>
+              <div className="config-day-header">
+                <p>Day {i + 1} </p>{" "}
+                <button
+                  onClick={async (e) => {
+                    const token = localStorage.getItem("auth-token");
+                    const response = await axios.put(
+                      `/api/courses/${match.params.id}/day`,
+                      { day: i + 1 },
+                      { headers: { "x-access-token": token } }
+                    );
+                    if (response.data && response.data.data) {
+                      setCourse(response.data.data);
+                    }
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
               <div className="config-material-title">
-                Title <input placeholder="enter the Title" />{" "}
+                Title{" "}
+                <input
+                  value={item.title}
+                  onChange={(e) => {
+                    if (!course) return;
+                    const c = { ...course };
+                    c.content[i].title = e.target.value;
+                    setCourse(c);
+                  }}
+                  placeholder="enter the Title"
+                />{" "}
               </div>
               <div className="config-material-content">
-                Material <textarea placeholder="enter the day material" />{" "}
+                Material{" "}
+                <textarea
+                  placeholder="enter the day material"
+                  value={item.material}
+                  onChange={(e) => {
+                    if (!course) return;
+                    const c = { ...course };
+                    c.content[i].material = e.target.value;
+                    setCourse(c);
+                  }}
+                />{" "}
               </div>
-              <button>save changes</button>
+              <button
+                className="config-save-changes"
+                onClick={async (e) => {
+                  const token = localStorage.getItem("auth-token");
+                  const response = await axios.put(
+                    `/api/courses/${match.params.id}`,
+                    { title: item.title, material: item.material, day: i + 1 },
+                    { headers: { "x-access-token": token } }
+                  );
+                }}
+              >
+                save changes
+              </button>
             </div>
           ))}
       </div>
